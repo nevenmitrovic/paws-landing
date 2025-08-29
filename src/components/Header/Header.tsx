11fdf78865d4b0167e791c107f62f6c21f967dcb
neven <nevenmitrovic4@gmail.com>
2025-08-29 22:10:36 +0200

    add mobile navlinks menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,18 @@
+import { useState } from 'react'
+
 import styles from './header.module.css'
 import logo from '@/assets/images/logo.png'
 import { HiMenuAlt3 } from 'react-icons/hi'
 import { FaAngleDown, FaAngleUp } from 'react-icons/fa'
+import { IoClose } from 'react-icons/io5'
+import { useIsMobile } from '@/hooks/useIsMobile'
 
 export default function Header() {
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+	const isMobile = useIsMobile()
+
+	const toggleMobileMenu = () => setIsMobileMenuOpen((prev) => !prev)
+
 	return (
 		<header className={styles.header}>
 			<div className={styles.content}>
@@ -14,8 +23,65 @@ export default function Header() {
 				</div>
 
 				{/* NAVLINKS */}
-				<nav>
-					<ul className={styles.navMenu}>
+				{!isMobile && (
+					<nav>
+						<ul className={styles.navMenu}>
+							<li>
+								<button>
+									Shop <FaAngleDown size={20} />
+								</button>
+							</li>
+							<li>
+								<button>
+									Services <FaAngleDown size={20} />
+								</button>
+							</li>
+							<li>
+								<button>About us</button>
+							</li>
+							<li>
+								<button>Blog</button>
+							</li>
+							<li>
+								<button>Contact</button>
+							</li>
+						</ul>
+					</nav>
+				)}
+			</div>
+
+			{/* NAVIGATION ACTION */}
+			{!isMobile && (
+				<div>
+					<button className={styles.navAction}>GET STARTED</button>
+				</div>
+			)}
+
+			{/* MOBILE MENU BUTTON */}
+			{isMobile && (
+				<button className={styles.mobileMenu} onClick={toggleMobileMenu}>
+					<HiMenuAlt3 size={30} />
+				</button>
+			)}
+
+			{/* MOBILE MENU CONTENT */}
+			{isMobile && (
+				<nav
+					className={`${styles.mobileMenuContent} ${isMobileMenuOpen ? styles.open : styles.close}`}
+				>
+					{/* MOBILE MENU HEADER */}
+					<div className={styles.mobileMenuContentHeader}>
+						<h3>
+							<img src={logo} alt='paws' width='30px' height='30px' />
+							Paws n’ Play
+						</h3>
+						<button onClick={toggleMobileMenu}>
+							<IoClose size={25} />
+						</button>
+					</div>
+
+					{/* MOBILE NAVLINKS */}
+					<ul>
 						<li>
 							<button>
 								Shop <FaAngleDown size={20} />
@@ -37,17 +103,7 @@ export default function Header() {
 						</li>
 					</ul>
 				</nav>
-			</div>
-
-			{/* NAVIGATION ACTION */}
-			<div>
-				<button className={styles.navAction}>GET STARTED</button>
-			</div>
-
-			{/* MOBILE MENU BUTTON */}
-			<button className={styles.mobileMenu}>
-				<HiMenuAlt3 size={30} />
-			</button>
+			)}
 		</header>
 	)
 }
