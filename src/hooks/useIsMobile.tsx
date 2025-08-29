7f140b61d75cb5185428ab512c58224ef87be007
neven <nevenmitrovic4@gmail.com>
2025-08-29 20:47:44 +0200

    add useIsMobile hook

---
+++ src/hooks/useIsMobile.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react'
+
+export const useIsMobile = (breakpoint = 768) => {
+	const [isMobile, setIsMobile] = useState(false)
+
+	useEffect(() => {
+		const checkDevice = () => {
+			setIsMobile(window.innerWidth < breakpoint)
+		}
+
+		checkDevice()
+		window.addEventListener('resize', checkDevice)
+		return () => window.removeEventListener('resize', checkDevice)
+	}, [breakpoint])
+
+	return isMobile
+}
