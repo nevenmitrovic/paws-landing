8d741774bb68e20323fde29926a7d0a5931da838
neven <nevenmitrovic4@gmail.com>
2025-09-01 20:13:30 +0200

    update header with change color on scroll

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 import styles from './header.module.css'
 import { useIsMobile } from '@/hooks/useIsMobile'
 import MobileMenu from '@/components/Header/MobileMenu/MobileMenu'
@@ -5,22 +7,47 @@ import Navigation from '@/components/Header/Navigation/Navigation'
 import Logo from '@/components/Header/Logo/Logo'
 
 export default function Header() {
+	const [color, setColor] = useState(false)
 	const isMobile = useIsMobile()
 
+	const handleColor = () => {
+		if (window.scrollY >= 90) {
+			setColor(true)
+		} else {
+			setColor(false)
+		}
+	}
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleColor)
+
+		return () => window.removeEventListener('scroll', handleColor)
+	}, [])
+
 	return (
-		<header className={styles.header}>
-			<Logo />
-			<div className={styles.content}>
-				<Navigation />
-			</div>
-			{!isMobile && (
-				<div>
-					<a href='#shop'>
-						<button className={styles.navAction}>GET STARTED</button>
-					</a>
+		<header className={`${styles.header} ${color ? styles.bgScroll : styles.bgInitial}`}>
+			{!isMobile ? (
+				<div className={styles.headerContent}>
+					<Logo />
+					<div className={styles.content}>
+						<Navigation />
+					</div>
+					<div>
+						<a href='#shop'>
+							<button className={styles.navAction}>GET STARTED</button>
+						</a>
+					</div>
+					<MobileMenu />
 				</div>
+			) : (
+				<>
+					<Logo />
+					<div className={styles.content}>
+						<Navigation />
+					</div>
+					<MobileMenu />
+				</>
 			)}
-			<MobileMenu />
 		</header>
 	)
 }
